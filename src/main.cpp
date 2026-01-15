29f1468d652169b4ab0b400b7958b49bb8ce8ff2
Task2401 <ana.chr.mr@gmail.com>
2026-01-15 02:59:35 +0200

    include the header files on Main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "../include/Simulation.h"
+#include "../include/GridWorld.h"
+#include "../include/VehicleSystem.h"
+#include "../include/WorldObjects.h"
+#include "../include/Common.h"
+
+using namespace std;
